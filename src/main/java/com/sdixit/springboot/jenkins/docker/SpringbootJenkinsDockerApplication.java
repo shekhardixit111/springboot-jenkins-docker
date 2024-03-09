24f63d4cae552dc3d39a8a154480fb6b8d741cbc
shekhar.dixit@cadienttalent.com <shekhar.dixit@cadienttalent.com>
2024-03-09 17:43:37 +0530

    Third checkin

--- src/main/java/com/sdixit/springboot/jenkins/docker/SpringbootJenkinsDockerApplication.java
+++ src/main/java/com/sdixit/springboot/jenkins/docker/SpringbootJenkinsDockerApplication.java
@@ -1,10 +1,21 @@
 package com.sdixit.springboot.jenkins.docker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringbootJenkinsDockerApplication {
+	public static Logger log=LoggerFactory.getLogger("SpringbootJenkinsDockerApplication");
+	
+	@PostConstruct
+	public void init() {
+		log.info("SpringbootJenkinsDockerApplication Postconstruct executed..............");
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
